5f97209f9608f4021c217e4a0de72a4a91320e00
jaswanth sannadi <jaswanth.sannadi@gmail.com>
2026-04-20 13:45:20 +0530

    uc7

--- src/main/java/com/example/helloapp/App.java
+++ src/main/java/com/example/helloapp/App.java
@@ -1,20 +1,10 @@
-
 public class App {
     public static void main(String[] args) {
 
         if (args.length == 0) {
             System.out.println("Hello World!");
         } else {
-            String result = "Hello ";
-
-            for (String name : args) {
-                result += name + " ";
-            }
-
-            // remove last space
-            result = result.substring(0, result.length() - 1);
-
-            System.out.println(result);
+            System.out.println("Hello " + String.join(" ", args));
         }
 
     }
